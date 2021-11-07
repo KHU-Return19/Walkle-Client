e7d43c0a64dc49e4dcfcfb495e5df576f443384b
MrMirror21 <mylittlesentry@gmail.com>
2021-11-07 14:49:14 +0900

    chore: init WalkleMapPage

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
+import WalkleMapPage from './pages/WalkleMapPage';
 
 const Router = () => {
     return (
         <Switch>
             <Route exact path="/" component={LandingPage} />
+            <Route exact path="/walklemap" component={WalkleMapPage} />
             <Route path="/">404 not found</Route>
         </Switch>
     )
