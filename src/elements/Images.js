8a623f3ec99497d9e76fb1e7729165d2f513f764
MrMirror21 <mylittlesentry@gmail.com>
2021-11-14 16:17:30 +0900

    chore: after code review

--- src/elements/Images.js
+++ src/elements/Images.js
@@ -1,41 +1,40 @@
 import React from "react";
 import styled from "styled-components";
 
-const Image = (props)=>{
-    const { src, width, height, margin } = props;
+const Image = (props) => {
+  const { src, width, height, margin } = props;
 
-    const styles ={
-        src: src,
-        width:width,
-        height:height,
-        margin:margin,
-    }
-    return (
-        <React.Fragment>
-            <ImageDefault {...styles}></ImageDefault>
-        </React.Fragment>
-       );
-    
-}
+  const styles = {
+    src: src,
+    width: width,
+    height: height,
+    margin: margin,
+  };
+  return (
+    <React.Fragment>
+      <ImageDefault {...styles}></ImageDefault>
+    </React.Fragment>
+  );
+};
 
 Image.defaultProps = {
-    src: "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/d583a565-afaa-4819-af46-f5bfdda677cd/Untitled.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20211113%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20211113T184000Z&X-Amz-Expires=86400&X-Amz-Signature=ae943c8a788529f748a42968e9823c05efcd270b98ecd691e05272c7a904243d&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22Untitled.png%22",
-    width: "100%",
-    height:"300px",
-    margin:"0",
-    };
+  src: "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/d583a565-afaa-4819-af46-f5bfdda677cd/Untitled.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20211113%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20211113T184000Z&X-Amz-Expires=86400&X-Amz-Signature=ae943c8a788529f748a42968e9823c05efcd270b98ecd691e05272c7a904243d&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22Untitled.png%22",
+  width: "100%",
+  height: "300px",
+  margin: "0",
+};
 
-    const ImageDefault = styled.div`
-    top:0;
-    width: ${(props)=>props.width};
-    height: ${(props)=>props.height};
-    background-image: url("${(props)=>props.src}");
-    background-size:cover;
-    background-position:center;
-    box-sizing:border-box;
-    margin:${(props)=>props.margin};
-    border-radius: 5%;
-    overflow: hidden;
-    `;
+const ImageDefault = styled.div`
+  top: 0;
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  background-image: url("${(props) => props.src}");
+  background-size: cover;
+  background-position: center;
+  box-sizing: border-box;
+  margin: ${(props) => props.margin};
+  border-radius: 5%;
+  overflow: hidden;
+`;
 
 export default Image;
--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -1,21 +1,16 @@
-import React from 'react'
-import Header from "../components/Header"
+import React from "react";
+import Header from "../components/Header";
 import styled from "styled-components";
 import Text from "../elements/Text.js";
 import Grid from "../elements/Grid.js";
 import Image from "../elements/Images.js";
 import SearchBar from "../components/SearchBar";
-
-//프로젝트 정렬을 해보려고 했는데 하려면 4개가 필요하더라고요 
-//주석쓰면서 생각이 난건데 그냥 4개 여기서 쓰고 자리배치 맞춰볼걸 그랬어요
-//마무리하고 일어나서 하려니까 왜 놓친게 계속 보이지??? 왼쪽 여백추가할게요
-// 정렬하는거는 아예 감도 못잡겠어서 마감임박순, 조회순 이거는 안 건드렸어요
+import { StyledText } from "../elements/Text.js";
 
 const ProjectPage = (props) => {
-
   return (
     <>
-      <Header  />
+      <Header />
       <SearchBar />
       <React.Fragment>
         <Grid
@@ -23,11 +18,11 @@ const ProjectPage = (props) => {
           height="300px"
           margin="10px 5px 50px 0"
           bg="#f9fafa"
-          border-radius= "5%"
-          overflow= "hidden"
+          border-radius="5%"
+          overflow="hidden"
           _onClick={() => {
             document.location.href = `/detail/${props.ProjectId}`;
-          } }
+          }}
         >
           <Image src={props.prj_img} width="100%" height="200px" />
           <Grid height="97px" padding="0 5%">
@@ -46,12 +41,24 @@ const ProjectPage = (props) => {
             </div>
           </Grid>
         </Grid>
-    </React.Fragment></>
+
+        <ProjectCard>
+          <Image src={props.prj_img} width="100%" height="200px" />
+          <ProjectInfo width="30%">
+            <InfoContainer>
+              <StyledText>{props.prj_writer}</StyledText>
+              <ListName>{props.prj_name}</ListName>
+            </InfoContainer>
+          </ProjectInfo>
+        </ProjectCard>
+      </React.Fragment>
+    </>
   );
 };
 
 ProjectPage.defaultProps = {
-  prj_img: "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/d583a565-afaa-4819-af46-f5bfdda677cd/Untitled.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20211113%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20211113T184000Z&X-Amz-Expires=86400&X-Amz-Signature=ae943c8a788529f748a42968e9823c05efcd270b98ecd691e05272c7a904243d&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22Untitled.png%22",
+  prj_img:
+    "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/d583a565-afaa-4819-af46-f5bfdda677cd/Untitled.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20211113%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20211113T184000Z&X-Amz-Expires=86400&X-Amz-Signature=ae943c8a788529f748a42968e9823c05efcd270b98ecd691e05272c7a904243d&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22Untitled.png%22",
   prj_name: "Project1",
   prj_writer: "user id",
   new: false,
@@ -69,6 +76,27 @@ const ListName = styled.p`
   width: 274px;
 `;
 
-
-
 export default ProjectPage;
+
+//참고용
+const ProjectCard = styled.div`
+  width: 300px;
+  height: 300px;
+  margin: 10px 5px 50px 0;
+  bg: #7054ff;
+  border-radius: 5%;
+  overflow: hidden;
+`;
+
+const ProjectInfo = styled.div`
+  height: 97px;
+  padding: 0 5%;
+  background: #ffffff;
+`;
+
+const InfoContainer = styled.div`
+  overflow: hidden;
+  textoverflow: ellipsis;
+  whitespace: nowrap;
+  width: 272px;
+`;
