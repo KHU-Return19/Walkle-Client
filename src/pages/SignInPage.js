c7fe4f8f69ff88a58b3055e103dc5c4c75cbc96a
MrMirror21 <49388937+MrMirror21@users.noreply.github.com>
2022-03-06 23:59:39 +0900

    feat: 로그인 연동

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -3,51 +3,64 @@ import axios from "axios";
 import Header from "../components/Header";
 import SignInForm from "../components/SignInForm";
 import { useRecoilState } from "recoil";
-import { userProfileState } from "../store/state";
+import { userProfileState, userIDState } from "../store/state";
+require("dotenv").config();
+
+const SERVER_ADDRESS = process.env.REACT_APP_SERVER_ADDRESS;
 
 const SignInPage = (props) => {
   const [id, setId] = useState("");
   const [password, setPassword] = useState("");
   const [userProfile, setUserProfile] = useRecoilState(userProfileState);
+  const [userID, setUserID] = useRecoilState(userIDState);
 
   const handleInput = (type) => (event) => {
     const targetVal = event.currentTarget.value;
     type === "id" ? setId(targetVal) : setPassword(targetVal);
   };
 
-  const onSubmitHandler = (event) => {
-    event.preventDefault();
-    let body = {
-      id: id,
-      password: password,
-    };
+  const getProfile = () => {
     axios
-      .post("/api/user/login", body)
+      .get(`http://${SERVER_ADDRESS}/api/profile/`, userID)
       .then((res) => {
-        if (res.data.success) {
-          axios.get("/api/profile/:nickname", body).then((res) => {
-            res.data.success
-              ? setUserProfile(res.data.user_data)
-              : alert(res.data.msg);
-          });
-          props.history.push("/");
-        } else {
-          alert(res.data.msg);
-        }
+        res.status >= 200 && res.status < 300
+          ? setUserProfile(res.data)
+          : alert(res.data.msg);
       })
       .catch((error) => {
         console.log(error);
       });
   };
 
+  const login = async (event) => {
+    event.preventDefault();
+    let body = {
+      loginId: id,
+      password: password,
+    };
+    try {
+      const { data } = await axios.post(
+        `http://${SERVER_ADDRESS}/api/users/login`,
+        body
+      );
+      setUserID(data);
+      alert("로그인이 완료되었습니다.");
+      //getProfile();
+      props.history.push("/");
+    } catch (error) {
+      alert(error);
+      console.log(error);
+    }
+  };
+
   return (
     <>
-      <Header userId="" />
+      <Header />
       <SignInForm
         id={id}
         password={password}
         handleInput={handleInput}
-        onSubmitHandler={onSubmitHandler}
+        onSubmitHandler={login}
       />
     </>
   );
